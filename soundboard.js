ec96bf429a7d2b17de427feb7f651609fb9410e9
Salman Osmani <osmanisa@msu.edu>
2021-11-18 23:42:33 -0500

    Audio change

--- soundboard.js
+++ soundboard.js
@@ -4,3 +4,12 @@ function play() {
   audio.play();
 }
 
+function play() {
+    var audio2 = document.getElementById("audio2");
+    audio2.play();
+  }
+
+  function play() {
+    var audio3 = document.getElementById("audio3");
+    audio3.play();
+  }
