408558252fe6c7ad5a88ba0d8669aa8a5d067faa
Salman Osmani <osmanisa@msu.edu>
2021-11-19 00:09:09 -0500

    audio fix

--- soundboard.js
+++ soundboard.js
@@ -4,12 +4,12 @@ function play() {
   audio.play();
 }
 
-function play2() {
+function play() {
   var audio2 = document.getElementById('play');
-  audio2.play2();
+  audio2.play();
 }
 
-function play3() {
+function play() {
   var audio3 = document.getElementById("audio3");
-  audio3.play3();
+  audio3.play();
 }
