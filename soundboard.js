d8b0e1a956e88405c515001ff16be4803e3fab63
Salman Osmani <osmanisa@msu.edu>
2021-11-19 00:06:15 -0500

    audio edit

--- soundboard.js
+++ soundboard.js
@@ -2,10 +2,14 @@
 function play() {
   var audio = document.getElementById("lightsaber");
   audio.play();
+}
 
+function play2() {
   var audio2 = document.getElementById('play');
   audio2.play2();
+}
 
+function play3() {
   var audio3 = document.getElementById("audio3");
   audio3.play3();
 }
