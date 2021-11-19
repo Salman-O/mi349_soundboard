fbc089145f98bd869e4548bedec77a31bbd3ab12
Salman Osmani <osmanisa@msu.edu>
2021-11-18 23:20:18 -0500

    buttons

--- soundboard.js
+++ soundboard.js
@@ -3,3 +3,11 @@ const button = document.querySelector('button');
 button.addEventListener('click', event => {
   button.textContent = `Click count: ${event.detail}`;
 });
+
+
+var buttonElement = document.getElementById('my-first-button')
+
+buttonElement.addEventListener("click", playAudio); {
+    console.log('I was clicked!')
+  }
+  
